8c9e8bcca4881e7d119255f1b06a3eed7ef892c4
Ravi Chandran <ravi.chandran@stackroute.in>
2018-11-15 23:46:06 +0530

    WHat are RXjs Operators. Learn the Map Operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { timer, fromEvent, Observable, noop  } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'about',
@@ -10,6 +13,35 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //  const interval$ = timer(3000, 1000);
+
+    //  const sub = interval$.subscribe(val => console.log('stream 1 ' + val));
+
+    //   setTimeout(() => sub.unsubscribe(), 6000);
+
+    //  const click$ = fromEvent(document, 'click');
+
+    //  click$.subscribe(
+    //    evt => console.log(evt),
+
+    //    err => console.log(err),
+
+    //    () => console.log('completed')
+    //  );
+
+    const http$ = createHttpObservable('/api/courses');
+
+    const courses$ = http$.pipe(
+      map( res => Object.values(res['payload']))
+    );
+
+    courses$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
   }
 
 }
+
+
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,21 @@
+import { Observable } from 'rxjs';
 
+export function createHttpObservable(url: string): Observable<any> {
+  return Observable.create(observer => {
+    // observer.next();
+    // observer.error();
+    // observer.complete();
+    fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(body => {
+            observer.next(body);
+            observer.complete();
+          })
+          .catch(err => {
+            observer.error(err);
+          });
+    });
+}
 
